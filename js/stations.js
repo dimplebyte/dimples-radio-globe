a04f617753944042cd153a5ecec3e57240f6ca62
DIMPLE KHANGAROT <dimplekhangarot7773@gmail.com>
2026-08-13 23:17:13 +0530

    Add initial list of radio stations

---
+++ js/stations.js
@@ -0,0 +1,195 @@
+const stations = [
+
+    {
+        id: 1,
+
+        name: "Radio Globe India",
+
+        city: "Jaipur",
+
+        country: "India",
+
+        latitude: 26.9124,
+
+        longitude: 75.7873,
+
+        stream: ""
+    },
+
+    {
+        id: 2,
+
+        name: "Kishangarh Radio",
+
+        city: "Kishangarh",
+
+        country: "India",
+
+        latitude: 26.5906,
+
+        longitude: 74.8539,
+
+        stream: ""
+    },
+
+    {
+        id: 3,
+
+        name: "Delhi Radio",
+
+        city: "New Delhi",
+
+        country: "India",
+
+        latitude: 28.6139,
+
+        longitude: 77.2090,
+
+        stream: ""
+    },
+
+    {
+        id: 4,
+
+        name: "Mumbai Radio",
+
+        city: "Mumbai",
+
+        country: "India",
+
+        latitude: 19.0760,
+
+        longitude: 72.8777,
+
+        stream: ""
+    },
+
+    {
+        id: 5,
+
+        name: "London Radio",
+
+        city: "London",
+
+        country: "United Kingdom",
+
+        latitude: 51.5074,
+
+        longitude: -0.1278,
+
+        stream: ""
+    },
+
+    {
+        id: 6,
+
+        name: "Paris Radio",
+
+        city: "Paris",
+
+        country: "France",
+
+        latitude: 48.8566,
+
+        longitude: 2.3522,
+
+        stream: ""
+    },
+
+    {
+        id: 7,
+
+        name: "Tokyo Radio",
+
+        city: "Tokyo",
+
+        country: "Japan",
+
+        latitude: 35.6762,
+
+        longitude: 139.6503,
+
+        stream: ""
+    },
+
+    {
+        id: 8,
+
+        name: "New York Radio",
+
+        city: "New York",
+
+        country: "United States",
+
+        latitude: 40.7128,
+
+        longitude: -74.0060,
+
+        stream: ""
+    },
+
+    {
+        id: 9,
+
+        name: "Sydney Radio",
+
+        city: "Sydney",
+
+        country: "Australia",
+
+        latitude: -33.8688,
+
+        longitude: 151.2093,
+
+        stream: ""
+    },
+
+    {
+        id: 10,
+
+        name: "Rio Radio",
+
+        city: "Rio de Janeiro",
+
+        country: "Brazil",
+
+        latitude: -22.9068,
+
+        longitude: -43.1729,
+
+        stream: ""
+    },
+
+    {
+        id: 11,
+
+        name: "Cairo Radio",
+
+        city: "Cairo",
+
+        country: "Egypt",
+
+        latitude: 30.0444,
+
+        longitude: 31.2357,
+
+        stream: ""
+    },
+
+    {
+        id: 12,
+
+        name: "Dubai Radio",
+
+        city: "Dubai",
+
+        country: "United Arab Emirates",
+
+        latitude: 25.2048,
+
+        longitude: 55.2708,
+
+        stream: ""
+    }
+
+];
